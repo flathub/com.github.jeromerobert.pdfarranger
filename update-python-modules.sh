49844bb51cb8698f26b76ea33ff06ac34c12a283
Hubert Figuière <hub@figuiere.net>
2021-10-30 17:00:49 -0400

    Automate python module generation

---
+++ update-python-modules.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+PIP_GENERATOR=flatpak-builder-tools/pip/flatpak-pip-generator
+
+$PIP_GENERATOR lxml
+# lxml must be force installed as it is in the GMOME 41 sdk but not the platform.
+sed -e 's/--no-build-isolation/--no-build-isolation --ignore-installed/g' -i python3-lxml.json
+
+$PIP_GENERATOR python-dateutil
+$PIP_GENERATOR pillow pybind11 setuptools-scm-git-archive setuptools-scm --output=python3-pikepdf-deps
